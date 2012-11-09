34ed2244697827837441c0bccd8d39ee0e86860e
SolidCoast <sc@solidcoast.org>
2012-11-08 23:47:24 -0500

    Removed unused references

--- Areas/Manager/Content/Js/Script.ashx.cs
+++ Areas/Manager/Content/Js/Script.ashx.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Web;
 
 using Piranha.Web;
-using Yahoo.Yui.Compressor;
 
 namespace Piranha.Areas.Manager.Content.Js
 {
