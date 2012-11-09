250ede4032e5c9e79118ec507aa2791ac043805d
SolidCoast <sc@solidcoast.org>
2012-11-08 23:51:48 -0500

    Removed unused references

--- Web/ResourceHandler.cs
+++ Web/ResourceHandler.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
 using System.Web;
-
-using Piranha.Web;
-using Yahoo.Yui.Compressor;
 
 namespace Piranha.Web
 {
